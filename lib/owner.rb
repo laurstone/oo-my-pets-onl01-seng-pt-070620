4a0a5b99f950c89ae4137d4e0923d082008240f0
Lauren Stone <lstone321@gmail.com>
2020-08-11 22:02:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,63 +1,50 @@
-require "pry"
-
 class Owner
+attr_accessor :cats, :dogs
   attr_reader :name, :species
-  attr_accessor :dogs, :cats
-  
-  
-  @@all = []
-  
+
+  @@owners = []
+
+# instance methods
+
   def initialize(name)
     @name = name
     @species = "human"
-    @dogs = []
+    @@owners << self
     @cats = []
-    @@all << self
-
+    @dogs = []
   end
 
   def say_species()
     "I am a #{species}."
   end
-  
-  def self.all 
-    @@all
+
+  def cats()
+    @cats
   end
-  
-  def self.count
-    @@all.count
+
+  def dogs()
+    @dogs
   end
-  
-  def self.reset_all
-    @@all.clear
+
+  def buy_cat(cat)
+    new_cat = Cat.new(cat,self)
+    @cats.push(new_cat) unless @cats.include?(new_cat)
   end
-  
-  def cats
-    Cat.all.select {|cat| cat.owner == self }
+
+  def buy_dog(dog)
+    new_dog = Dog.new(dog,self)
+    @dogs.push(new_dog) unless @dogs.include?(new_dog)
   end
-  
-  def dogs 
-    Dog.all.select {|dog| dog.owner == self }
-  end 
-  
-  def buy_cat(cat_name)
-  Cat.new(cat_name, self)
-  end
-  
-  def buy_dog(dog_name)
-  Dog.new(dog_name, self)
-  end
-  
+
   def walk_dogs
     @dogs.each {|dog| dog.mood = "happy"}
   end
-  
+
   def feed_cats
     @cats.each {|cat| cat.mood = "happy"}
-  end 
-  
-  
-   def sell_pets
+  end
+
+  def sell_pets
     @dogs.each do |dog|
       dog.mood = "nervous"
       dog.owner = nil
@@ -70,10 +57,22 @@ class Owner
     end
   end
 
-    
-  
-  def self.reset_all
-    @@all = []
-  end 
-  
+  def list_pets
+    "I have #{@dogs.count} dog(s), and #{@cats.count} cat(s)."
+  end
+
+#class methods
+
+  def self.all()
+    @@owners
+  end
+
+  def self.count()
+    @@owners.count
+  end
+
+  def self.reset_all()
+    @@owners = []
+  end
+
 end
